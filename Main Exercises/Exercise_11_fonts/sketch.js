54049729007031d0f8af511b3f656e5a86177047
Eoan O Dea <odeaeoan@gmail.com>
2021-03-03 11:02:07 +0000

    Implemented font and rendered it to the canvas

--- Main Exercises/Exercise_11_fonts/sketch.js
+++ Main Exercises/Exercise_11_fonts/sketch.js
@@ -12,10 +12,51 @@
  * Copyright 2021 WebSpace, WebSpace
  */
 
+let font;
+let path;
+let fontPath;
+let tracker = 0;
+
 function setup() {
   createCanvas(window.innerWidth, window.innerHeight);
+  background(255);
+  opentype.load("./assets/fonts/DotGothic16-Regular.otf", function (err, f) {
+    if (err) return console.log("Error! ", err);
 
-  background(0);
+    font = f;
+  });
 }
 
-function draw() {}
+function draw() {
+  if (!font) return;
+
+  translate(0, 500);
+
+  fontPath = font.getPath("iadt", 0, 0, 300);
+
+  path = new g.Path(fontPath.commands);
+  path = g.resampleByLength(path, 30);
+  pathArray = path.commands;
+
+  // beginShape();
+  // for (let i = 0; i < fontPath.commands.length; i++) {
+  //   fill(0, 255, 0);
+  //   noStroke();
+  //   // const element = fontPath.commands[i];
+  //   vertex(fontPath.commands[i].x, fontPath.commands[i].y);
+  // }
+  // endShape();
+
+  beginShape();
+  for (let i = tracker; i < pathArray.length; i++) {
+    if (pathArray[i].type === "Z") {
+      tracker = i + 1;
+      break;
+    } else {
+      // fill(255, 0, 0);
+      // noStroke();
+      vertex(pathArray[i].x, pathArray[i].y);
+    }
+  }
+  endShape(CLOSE);
+}
