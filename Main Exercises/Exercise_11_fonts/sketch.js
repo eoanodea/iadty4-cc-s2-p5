83326275c3ec0acf3dfc4bb93874129c2c945b81
Eoan O Dea <odeaeoan@gmail.com>
2021-03-03 09:38:27 +0000

    Added Exercise 11 - fonts

---
+++ Main Exercises/Exercise_11_fonts/sketch.js
@@ -0,0 +1,21 @@
+/**
+ * File: sketch.js
+ * Project: Exercise_02_random_walking_extended
+ * Version <<projectversion>>
+ * File Created: Wednesday, 20th January 2021 9:40:55 am
+ * Author: Eoan O'Dea (eoan@web-space.design)
+ * -----
+ * File Description:
+ * Last Modified: Friday, 12th February 2021 4:38:47 pm
+ * Modified By: Eoan O'Dea (eoan@web-space.design>)
+ * -----
+ * Copyright 2021 WebSpace, WebSpace
+ */
+
+function setup() {
+  createCanvas(window.innerWidth, window.innerHeight);
+
+  background(0);
+}
+
+function draw() {}
